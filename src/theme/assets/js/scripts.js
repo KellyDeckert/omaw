5fe678c32c95e7b78048b26e6c99054aa46c3e71
JessJudd <jessjudd25@gmail.com>
2022-09-12 16:08:30 -0700

    Fix Footer Mobile Clicking
    Fixed menu closing instead of firing navigation links

--- src/theme/assets/js/scripts.js
+++ src/theme/assets/js/scripts.js
@@ -9,37 +9,47 @@ ready(() => {
 });
 
 function initMobileFooterAccordions(){
-	var accordion = document.querySelectorAll('footer .menu li');
+	var accordion = document.querySelectorAll('footer .menu li.menu-item-has-children a');
 	
 	for (var i = 0; i<accordion.length; i++){
         accordion[i].addEventListener("click", (e) => { accordionClick(e) });
     }
 
 	var accordionClick = (eventHappened) => {
-		eventHappened.preventDefault();
 		var targetClicked = event.currentTarget;
-			if(!targetClicked.classList.contains('open')){
-				targetClicked.classList.add('open');
-			} else {
-				targetClicked.classList.remove('open');
-			}
-		var submenu = targetClicked.children[1];
-		if ( submenu.style.maxHeight ){
-			console.log( "current height: " + submenu.style.maxHeight );
-			submenu.style.maxHeight = null;
+		var classes = targetClicked.parentElement.classList;
+		var submenu = targetClicked.nextElementSibling;
+		
+		if(!classes.contains('open')){
+			classes.add('open');
 		} else {
-			var allSubMenus = document.querySelectorAll('footer .menu .sub-menu-wrapper');
-			console.log( allSubMenus );
-			for (var i = 0; i < allSubMenus.length; i++){
-				console.log("current menu: " + allSubMenus[i]);
-				var toggle = document.querySelectorAll('footer .menu .menu-item-has-children');
-				if (allSubMenus[i].style.maxHeight){
-						console.log("there is a menu already open");
-					allSubMenus[i].style.maxHeight = null;
-					toggle[i].classList.remove('open');
-				}
+			classes.remove('open');
+		}
+
+		if ( submenu && submenu.classList.contains('sub-menu-wrapper') ) {
+			if ( submenu.style.maxHeight ){
+				console.log( "current height: " + submenu.style.maxHeight );
+				submenu.style.maxHeight = null;
+			} else {
+
+				// ->> TO FIX, AUTOCLOSE OPEN MENU WHEN DIFFERENT MENU IS CLICKED
+
+				// var allSubMenus = document.querySelectorAll('footer .menu .sub-menu-wrapper');
+				// console.log( allSubMenus );
+				// for (var i = 0; i < allSubMenus.length; i++){
+				// 	console.log("current menu: " + allSubMenus[i]);
+				// 	var toggle = document.querySelectorAll('footer .menu .menu-item-has-children');
+				// 	if (allSubMenus[i].style.maxHeight){
+				// 			console.log("there is a menu already open");
+				// 		allSubMenus[i].style.maxHeight = null;
+				// 		toggle[i].classList.remove('open');
+				// 	}
+				// }
+
+				submenu.style.maxHeight = submenu.scrollHeight + "px";
 			}
-			submenu.style.maxHeight = submenu.scrollHeight + "px";
+		} else {
+			console.log('something was clicked but it was not the accordion parent');
 		}
 	}
 }
