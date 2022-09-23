e40f52abb8f1599f0aac253f41eaad68e5c36100
JessJudd <jessjudd25@gmail.com>
2022-09-23 13:52:27 -0700

    Yellow CTA with Image --> Done

--- src/theme/assets/acf/fields/about.php
+++ src/theme/assets/acf/fields/about.php
@@ -31,66 +31,106 @@ if (function_exists('acf_add_local_field_group')) :
     'fields' => array(
 
 
-      /**
-       * Our Story
-       */
-      array(
-        'key' => 'about__hero_tab',
-        'label' => 'Our Story',
-        'type' => 'tab',
-      ),
-      array(
-        'key' => 'about__story_display',
-        'label' => 'Display',
-        'name' => 'story_display',
-        'type' => 'true_false',
-        'message' => 'Check to display this section'
-      ),
-      array(
-        'key' => 'about__story_image',
-        'label' => 'Image',
-        'name' => 'story_image',
-        'type' => 'image',
-        'return_format' => 'array',
-      ),
-      array(
-        'key' => 'about__story_image_tile',
-        'label' => 'Extended Image Tile',
-        'name' => 'story_image_tile',
-        'type' => 'image',
-        'return_format' => 'array',
-      ),
-      array(
-        'key' => 'about__story_label',
-        'label' => 'Section Label',
-        'name' => 'story_label',
-        'type' => 'text',
-        'default_value' => 'Our Story',
-      ), 
-      array(
-        'key' => 'about__story_heading',
-        'label' => 'Heading',
-        'name' => 'story_heading',
-        'type' => 'text',
-      ),     
-      array(
-        'key' => 'about__story_content',
-        'label' => 'Content',
-        'name' => 'story_content',
-        'type' => 'wysiwyg',
-        'toolbar' => 'basic',
-        'media_upload' => 0,
-      ),
-      array(
-        'key' => 'about__story_overlay_content',
-        'label' => 'Overlay Content',
-        'name' => 'story_overlay_content',
-        'type' => 'wysiwyg',
-        'toolbar' => 'basic',
-        'media_upload' => 0,
-      ), 
+/*  OUR STORY
+============================= */
+  array(
+    'key' => 'about__hero_tab',
+    'label' => 'Our Story',
+    'type' => 'tab',
+  ),
+  array(
+    'key' => 'about__story_display',
+    'label' => 'Display',
+    'name' => 'story_display',
+    'type' => 'true_false',
+    'message' => 'Check to display this section'
+  ),
+  array(
+    'key' => 'about__story_image',
+    'label' => 'Image',
+    'name' => 'story_image',
+    'type' => 'image',
+    'return_format' => 'array',
+  ),
+  array(
+    'key' => 'about__story_image_tile',
+    'label' => 'Extended Image Tile',
+    'name' => 'story_image_tile',
+    'type' => 'image',
+    'return_format' => 'array',
+  ),
+  array(
+    'key' => 'about__story_label',
+    'label' => 'Section Label',
+    'name' => 'story_label',
+    'type' => 'text',
+    'default_value' => 'Our Story',
+  ), 
+  array(
+    'key' => 'about__story_heading',
+    'label' => 'Heading',
+    'name' => 'story_heading',
+    'type' => 'text',
+  ),     
+  array(
+    'key' => 'about__story_content',
+    'label' => 'Content',
+    'name' => 'story_content',
+    'type' => 'wysiwyg',
+    'toolbar' => 'basic',
+    'media_upload' => 0,
+  ),
+  array(
+    'key' => 'about__story_overlay_content',
+    'label' => 'Overlay Content',
+    'name' => 'story_overlay_content',
+    'type' => 'wysiwyg',
+    'toolbar' => 'basic',
+    'media_upload' => 0,
+  ),
+    
+/*  YELLOW CTA
+============================= */
+  array(
+    'key' => 'about__yellow-cta__tab',
+    'label' => 'CTA',
+    'type' => 'tab',
+  ),
+  array(
+    'key' => 'about__yellow-cta__display',
+    'label' => 'Display',
+    'name' => 'yellow-cta__display',
+    'type' => 'true_false',
+    'message' => 'Check to display this CTA'
+  ),
+  array(
+    'key' => 'about__yellow-cta__heading',
+    'label' => 'Heading',
+    'name' => 'yellow-cta__heading',
+    'type' => 'text',
+  ), 
+  array(
+    'key' => 'about__yellow-cta__content',
+    'label' => 'Content',
+    'name' => 'yellow-cta__content',
+    'type' => 'wysiwyg',
+    'toolbar' => 'basic',
+    'media_upload' => 0,
+  ),
+  array(
+    'key' => 'about__yellow-cta__link',
+    'label' => 'CTA Link',
+    'name' => 'yellow-cta__link',
+    'type' => 'link',
+  ),
+  array(
+    'key' => 'about__yellow-cta__img',
+    'label' => 'CTA Image',
+    'name' => 'yellow-cta__image',
+    'type' => 'image',
+    'return_format' => 'array',
+  ),
 
-    ),
-  ));
-
+),
+));
 endif;
--- src/theme/page-templates/page-about.php
+++ src/theme/page-templates/page-about.php
@@ -23,20 +23,6 @@ get_header();
     </div>
 
     <?php 
-    // get_template_part( 'sections/section-our-story-content.php', null,
-    //     array( 
-    //         'data'  => array(
-    //             'id' => 'our-story',
-    //             'display' => get_field('story_display'),
-    //             'image' => get_field('story_image'),
-    //             'image_tile' => get_field('story_image_tile'),
-    //             'label' => get_field('story_label'),
-    //             'heading' => get_field('story_heading'),
-    //             'content' => get_field('story_content'),
-    //             'overlay_content' => get_field('story_overlay_content'),
-    //         )
-    //     )
-    // ); 
     get_template_part( 'sections/section','our-story-content', 
         array( 
             'data'  => array(
@@ -66,9 +52,22 @@ get_header();
         <h2>Image Float Left</h2>
     </div>
 
-    <div class="section-placeholder">
-        <h2>Yellow CTA w/ image</h2>
-    </div>
+    <?php 
+    get_template_part( 'sections/section','yellow-cta-w-img', 
+        array( 
+            'data'  => array(
+                'id' => 'become-a-member',
+                'full_width' => false,
+                'display' => get_field('yellow-cta__display'),
+                'heading' => get_field('yellow-cta__heading'),
+                'sub_heading' => get_field('yellow-cta__sub_heading'),
+                'content' => get_field('yellow-cta__content'),
+                'cta' => get_field('yellow-cta__link'),
+                'image' => get_field('yellow-cta__image'),
+            )
+        )
+    );
+    ?>
 
 </div>
 
