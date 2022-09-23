d5a908372e27118ce4510caac2c1d8ac070098d0
JessJudd <jessjudd25@gmail.com>
2022-09-23 13:01:36 -0700

    About Page ACF hooked up

--- src/theme/assets/acf/fields/about.php
+++ src/theme/assets/acf/fields/about.php
@@ -10,7 +10,7 @@ if (function_exists('acf_add_local_field_group')) :
         array(
           'param' => 'page_template',
           'operator' => '==',
-          'value' => 'about.php',
+          'value' => 'page-templates/page-about.php',
         ),
       ),
     ),
@@ -29,6 +29,66 @@ if (function_exists('acf_add_local_field_group')) :
       11 => 'send-trackbacks',
     ),
     'fields' => array(
+
+
+      /**
+       * Our Story
+       */
+      array(
+        'key' => 'about__hero_tab',
+        'label' => 'Our Story',
+        'type' => 'tab',
+      ),
+      array(
+        'key' => 'about__story_display',
+        'label' => 'Display',
+        'name' => 'story_display',
+        'type' => 'true_false',
+        'message' => 'Check to display this section'
+      ),
+      array(
+        'key' => 'about__story_image',
+        'label' => 'Image',
+        'name' => 'story_image',
+        'type' => 'image',
+        'return_format' => 'array',
+      ),
+      array(
+        'key' => 'about__story_image_tile',
+        'label' => 'Extended Image Tile',
+        'name' => 'story_image_tile',
+        'type' => 'image',
+        'return_format' => 'array',
+      ),
+      array(
+        'key' => 'about__story_label',
+        'label' => 'Section Label',
+        'name' => 'story_label',
+        'type' => 'text',
+        'default_value' => 'Our Story',
+      ), 
+      array(
+        'key' => 'about__story_heading',
+        'label' => 'Heading',
+        'name' => 'story_heading',
+        'type' => 'text',
+      ),     
+      array(
+        'key' => 'about__story_content',
+        'label' => 'Content',
+        'name' => 'story_content',
+        'type' => 'wysiwyg',
+        'toolbar' => 'basic',
+        'media_upload' => 0,
+      ),
+      array(
+        'key' => 'about__story_overlay_content',
+        'label' => 'Overlay Content',
+        'name' => 'story_overlay_content',
+        'type' => 'wysiwyg',
+        'toolbar' => 'basic',
+        'media_upload' => 0,
+      ), 
 
     ),
   ));
--- src/theme/page-templates/page-about.php
+++ src/theme/page-templates/page-about.php
@@ -22,39 +22,37 @@ get_header();
         <h2>Two Column Copy Block</h2>
     </div>
 
-    <section class="content-story extend-bg color-brand-primary">
-        <div class="content-story__columns">
-            <div class="content-story__content" data-aos="smooth-slide-up" data-aos-delay="250" data-aos-duration="500" data-aos-easing="cubic">
-                <h5>Our Story</h5>
-                <h2>In 2017, One Mind launched One Mind at Work to focus on the unique opportunities and challenges in supporting workplace mental health.</h2>
-                <p>Most people access healthcare through their employers. Mental health is no different. For One Mind, addressing mental health within the workplace provides an opportunity to reach impact at scale while simultaneously helping businesses to thrive.</p>
-            </div>
-            <div class="content-story__img drawn-bg drawn_bg--right">
-                <div class="desktop content-story__image aos-init aos-animate" data-aos="smooth-slide-right" data-aos-delay="100" data-aos-duration="350">
-                    <div class="drawn_bg_wrapper">
-                        <img loading="lazy" src="http://omaw.local/wp-content/uploads/2022/09/Time.png" alt="">
-                        <span class="drawn_bg_tile" style="background-image:url(http://omaw.local/wp-content/uploads/2022/09/Time_LineOnly.png);"></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <div class="content-story__overlay">
-        <div class="story-overlay__gutter"></div>
-        <div class="story-overlay__container">
-            <div class="story-overlay__inner layout--1080" data-aos="smooth-slide-up" data-aos-delay="250" data-aos-duration="500" data-aos-easing="cubic">
-                <h3>Given that most of the world's population spends one-third of their adult lives at work, employers have a tremendous opportunity to improve quality of life for their employees and broader communities, and play a critical role in promoting mental health solutions.</h3>
-                <p>Moreover, leaving mental health needs unaddressed leads to costly results, including increased absenteeism, increased liability, and lost productivity. We support leaders who agree that there is both a moral imperative and a business imperative to investing in better mental health support for the workforce.</p>
-            </div>
-        </div>
-        <div class="content-story__img mobile">
-            <div class="mobile content-story__image aos-init aos-animate" data-aos="smooth-slide-right" data-aos-delay="100" data-aos-duration="350">
-                <div class="drawn_bg_wrapper">
-                    <img loading="lazy" src="http://omaw.local/wp-content/uploads/2022/09/Time.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php 
+    // get_template_part( 'sections/section-our-story-content.php', null,
+    //     array( 
+    //         'data'  => array(
+    //             'id' => 'our-story',
+    //             'display' => get_field('story_display'),
+    //             'image' => get_field('story_image'),
+    //             'image_tile' => get_field('story_image_tile'),
+    //             'label' => get_field('story_label'),
+    //             'heading' => get_field('story_heading'),
+    //             'content' => get_field('story_content'),
+    //             'overlay_content' => get_field('story_overlay_content'),
+    //         )
+    //     )
+    // ); 
+    get_template_part( 'sections/section','our-story-content', 
+        array( 
+            'data'  => array(
+                'id' => 'our-story',
+                'display' => get_field('story_display'),
+                'image' => get_field('story_image'),
+                'image_tile' => get_field('story_image_tile'),
+                'alt' => get_field('story_image_alt'),
+                'label' => get_field('story_label'),
+                'heading' => get_field('story_heading'),
+                'content' => get_field('story_content'),
+                'overlay_content' => get_field('story_overlay_content'),
+            )
+        )
+    );
+    ?>
 
     <div class="section-placeholder">
         <h2>Single Quote</h2>
