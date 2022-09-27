ad0ea82d8a8ce5a6259bd6e9816c04e50f676996
Pablo Santiago <pjsdesigns@yahoo.com>
2022-09-27 12:23:07 -0700

    Illustration header updated to work if empty

--- src/theme/sections/section-header-illustration.php
+++ src/theme/sections/section-header-illustration.php
@@ -5,13 +5,13 @@ $heading = isset($args['data']['heading']) ? $args['data']['heading'] : null;
 
 if( $display):
 ?>
-<section id="<?php echo $section_id?>" class="header-illustration  color-brand-primary">
-    <div class="header-illustration__inner section-h-padding layout--1440" data-aos="smooth-slide-down" data-aos-delay="250" data-aos-duration="400" data-aos-easing="cubic">
-        <div class="section-1080">
-        <?php
-            echo $heading ? '<h1 class="header-illustration__heading" data-aos="smooth-slide-left" data-aos-delay="450" data-aos-duration="700" data-aos-easing="cubic">'.$heading.'</h1>' : '';
-        ?>
-        </div>
-    </div>
+<section id="<?php echo $section_id?>" class="header-illustration  color-brand-primary <?php echo !$heading ? 'header-illustration--empty': '';?>">
+    <div class="header-illustration__inner section-h-padding layout--1440" data-aos="smooth-slide-down" data-aos-delay="250" data-aos-duration="400" data-aos-easing="cubic"><?php
+        if($heading){
+            echo '<div class="section-1080">';
+                echo '<h1 class="header-illustration__heading" data-aos="smooth-slide-left" data-aos-delay="450" data-aos-duration="700" data-aos-easing="cubic">'.$heading.'</h1>';
+            echo '</div>';
+        }
+    ?></div>
 </section>
 <?php endif; ?>
