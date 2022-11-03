da2275519235ddde80741b916797b862c957074b
JessJudd <jessjudd25@gmail.com>
2022-11-03 11:56:43 -0700

    Add favicon

--- src/theme/header.php
+++ src/theme/header.php
@@ -4,7 +4,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<!--<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">-->
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class('wip'); ?>>
