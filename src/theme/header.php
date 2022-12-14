a22d07863a124c0f5cf91ae00449ff957d14913d
Pablo Santiago <pjsdesigns@yahoo.com>
2022-12-14 15:00:35 -0800

    Google analytics moved up

--- src/theme/header.php
+++ src/theme/header.php
@@ -1,11 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
-	<?php wp_head(); ?>
   <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-VGZ130BYYL"></script>
   <script>
@@ -14,7 +9,13 @@
   gtag('js', new Date());
 
   gtag('config', 'G-VGZ130BYYL');
-  </script>
+  </script>  
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
+	<?php wp_head(); ?>
+
 </head>
 <body <?php body_class('wip'); ?>>
 
